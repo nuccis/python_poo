a432d8fc331e78de7cfdf5484b14f4405cdb7467
Leticia Nogueira <leticianogueira1@hotmail.com>
2024-03-01 15:50:49 -0400

    Iniciei o desafio 6
    A classe tarefa foi criada

--- desafios/des006.py
+++ desafios/des006.py
@@ -1,1 +1,28 @@
 #Sistema de Gerenciamento de Tarefas: Crie um aplicativo para gerenciar tarefas a fazer. Cada tarefa deve ter uma descrição, uma data de vencimento e um status (pendente, em andamento, concluída).
+from datetime import datetime
+from time import sleep
+
+#Classes
+class Tarefa:
+    statusPossiveis = ['Pendente', 'Em Andamento', 'Concluído']
+
+    def __init__(self, descricao:str, vencimento:datetime, status:str=statusPossiveis[0]) -> None:
+        self.descricao = descricao
+        self.criacao = datetime.today().date().isoformat()
+        self.vencimento = vencimento
+        self.status = status
+    
+    @property
+    def vencimento(self):
+        return self._vencimento
+    
+    @vencimento.setter
+    def vencimento(self, value):
+        self._vencimento = datetime.fromisoformat(value).date()
+
+
+#Programa principal
+tarefa01 = Tarefa('limparcasa','2024-03-02')
+print(tarefa01.criacao)
+print(tarefa01.vencimento)
+print(tarefa01.status)
